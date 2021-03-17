4a9afb0c30ffe76e0b8acb454e92cb6c012a18c2
Nachiappan-RM <65343089+Nachiappan-RM@users.noreply.github.com>
2021-03-17 19:11:30 +0530

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,10 @@
+// alert('Please give your feedback!')
+function post() {
+if(window.confirm("Are you sure you want to submit?"))
+{
+	window.open("https://formspree.io/thanks?next=https%3A%2F%2Fpriyanshi527.github.io%2Fhtml%2FPortfolio%2Fportfolio.html&language=en")
+}
+else {
+	window.open("https://priyanshi527.github.io/html/Portfolio/portfolio.html");
+}
+}
