9ecf326560ef9943dd18eb6e250f7d18c82be6ca
Nachiappan-RM <65343089+Nachiappan-RM@users.noreply.github.com>
2023-03-11 15:52:52 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,9 +2,9 @@
 function post() {
 if(window.confirm("Are you sure you want to submit?"))
 {
-	window.open("https://formspree.io/thanks?next=https%3A%2F%2Fpriyanshi527.github.io%2Fhtml%2FPortfolio%2Fportfolio.html&language=en")
+	window.open("https://formspree.io/thanks")
 }
 else {
-	window.open("https://priyanshi527.github.io/html/Portfolio/portfolio.html");
+	window.open("https://nachiappan.glitch.me/");
 }
 }
